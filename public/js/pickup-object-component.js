2b5885c5da91141347e4d8c72386bafaff023c9a
unknown <hockey23.97@gmail.com>
2020-01-23 22:45:11 -0500

    uploading changes

--- public/js/pickup-object-component.js
+++ public/js/pickup-object-component.js
@@ -1,24 +1,46 @@
+var Picked_UP = false;
+
 AFRAME.registerComponent( 'pickup-object-component',{
     init:function(){
         console.log("Init component");
 
         const Context_AF = this;
-        let Picked_Up = false;
+        
 
         Context_AF.el.addEventListener('click', function(event){
-            //var position_copy = ball.position;
+            
+            if(Picked_UP){
+                Context_AF.dropBall();
+            }
+            else{
+                Context_AF.pickUpObject();
+            }
+
+            /* 
+            var position_copy = ball.position;
+            console.log(Picked_UP);
             console.log('click');
             let camera = document.getElementById('camera');
             let ball = document.getElementById('ball');
-            //ball.object3D.position;
-            //const wVec = camera.object3D.localToWorld( ball.object3D.position );
+            position before pick up
+            console.log(ball.object3D.position);
+            const wVec = camera.object3D.localToWorld(ball.object3D.position);
+            camera.object3D.localToWorld(ball.object3D.position);
+            
             camera.object3D.add(ball.object3D);
-            ball.position = "0 1 0"
+            console.log(ball.object3D.position);
+            ball.setAttribute('position', {x: 0, y: 0, z:-2});
+            console.log(ball.object3D.position);
+            console.log(wVec);
+            position after pick up
+            console.log(wVec);
+            
             Picked_UP = true;
-            console.log(Picked_UP);
+            onsole.log(Picked_UP);
             
 
-            //Context_AF.createCow();
+            Context_AF.createCow();
+            */
         });
         Context_AF.el.addEventListener('mouseenter', function(event){
             //e1 = html enitity or element
@@ -47,5 +69,71 @@ AFRAME.registerComponent( 'pickup-object-component',{
         //add to scene
         let scene = document.querySelector('a-scene');
         scene.appendChild(grabElem);
+    },
+
+    dropObject : function() {
+        const Context_AF = this;
+        let camera = document.getElementById('camera');
+        let wVec = camera.object3D.localToWorld(Context_AF.el.object3D.position);
+        console.log(wVec);
+        console.log(parseFloat(wVec.x.toFixed(2)));
+        console.log(wVec.y);
+        console.log(wVec.z);
+        //let ball = document.getElementById('ball');
+        Context_AF.el.parentNode.removeChild(Context_AF.el);
+        console.log("dropping item");
+        Picked_UP = false;
+
+        // creating a new object in the position of the removed object
+        let grabElem = document.createElement('a-entity');
+        grabElem.setAttribute('position',{x: 0, y:1, z:0});
+        grabElem.setAttribute('id', 'ball'); 
+        grabElem.setAttribute('class', 'clickable');
+        grabElem.setAttribute('geometry',{primitive:'sphere', radius:'0.5', height:'1.0'});
+        grabElem.setAttribute('material','color:yellow;');
+        grabElem.setAttribute('pickup-object-component','');
+
+        let scene = document.querySelector('a-scene');
+        scene.appendChild(grabElem);
+    },
+
+    pickUpObject : function() {
+        const Context_AF = this;
+        let camera = document.getElementById('camera');
+        //let ball = document.getElementById('ball');
+
+        camera.object3D.localToWorld(Context_AF.el.object3D.position);
+        camera.object3D.add(Context_AF.el.object3D);
+        camera.object3D.add(ball.object3D)
+        Context_AF.el.setAttribute('position', {x: 0, y: 0, z:-2});
+        Picked_UP = true;
+        console.log("item picked up");
+        
+    },
+    dropBall :function(){
+        const Context_AF = this;
+        let camera = document.getElementById('camera');
+
+        let wVec = camera.object3D.localToWorld(Context_AF.el.object3D.position);
+
+        Context_AF.el.parentNode.removeChild(Context_AF.el);
+        console.log("placing item");
+        Picked_UP = false;
+
+        // creating a new object in the position of the removed object
+        let grabElem = document.createElement('a-entity');
+        grabElem.position = "0 1 0";
+        //grabElem.setAttribute('position',{x: wVec.x, y:wVec.y, z:wVec.z});
+        grabElem.setAttribute('id', 'ball'); 
+        grabElem.setAttribute('class', 'clickable');
+        grabElem.setAttribute('geometry',{primitive:'sphere', radius:'0.5', height:'1.0'});
+        grabElem.setAttribute('material','color:yellow;');
+        grabElem.setAttribute('pickup-object-component','');
+
+        let scene = document.querySelector('a-scene');
+        scene.appendChild(grabElem);
+
+
+        
     }
 });
