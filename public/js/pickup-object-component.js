d702287f8875b8ca85b550a2db91350e58be78f6
Alexzender Paromenskiy <paro0006@algonquinlive.com>
2020-01-24 11:53:19 -0500

    changes from class

--- public/js/pickup-object-component.js
+++ public/js/pickup-object-component.js
@@ -43,48 +43,28 @@ AFRAME.registerComponent( 'pickup-object-component',{
             */
         });
         Context_AF.el.addEventListener('mouseenter', function(event){
-            //e1 = html enitity or element
-            //object3D = three.js (rendering engine) 3D element
             //Context_AF.el.object3D.scale.set(1.1,1.1,1.1);
         });
         Context_AF.el.addEventListener('mouseleave', function(event){
             //Context_AF.el.object3D.scale.set(1.0,1.0,1.0);
         });
     },
-    createCow : function() {
-        const Context_AF = this;
-
-        let grabElem = document.createElement('a-entity');
-        grabElem.setAttribute('class','clickable');
-        //grabElem.setAttribute('obj-model', 'obj:assets/models/Cow.obj');
-        //grabElem.setAttribute('material', 'src:assets/textures/Cow.png');
-        grabElem.setAttribute('delete-cow-component','');
-
-        // random transforms
-        // grabElem.setAttribute('position', {x:(Math.random() * 6.0) - 3.0, y:0, z:-4 -(Math.random()*3.0)});
-        // const randScale = 0.2 + (Math.random() * 0.8);
-        // grabElem.setAttribute('scale',{x:randScale, y:randScale, z:randScale});
-        // grabElem.setAttribute('rotation',{x:0, y:Math.random() *360.0,z:0});
-
-        //add to scene
-        let scene = document.querySelector('a-scene');
-        scene.appendChild(grabElem);
-    },
 
     dropObject : function() {
-        let scene = document.querySelector('a-scene');
         const Context_AF = this;
+        let scene = document.querySelector('a-scene');
         let camera = document.getElementById('camera');
         let wVec = camera.object3D.localToWorld(Context_AF.el.object3D.position);
-        console.log(wVec);
-        console.log(parseFloat(wVec.x.toFixed(2)));
-        console.log(wVec.y);
-        console.log(wVec.z);
+       
         //let ball = document.getElementById('ball');
         //Context_AF.el.parentNode.removeChild(Context_AF.el);
         scene.object3D.add(Context_AF.el.object3D);
-        Context_AF.el.object3D.position = (wVec.x, wVec.y, wVec.z);
+        //Context_AF.el.object3D.position = (wVec.x, wVec.y, wVec.z);
+        Context_AF.el.setAttribute('position',{x:wVec.x, y:wVec.y, z:wVec.z});
+        Context_AF.el.setAttribute('dynamic-body', '');
 
+
+        
 
         console.log("dropping item");
         Picked_UP = false;
@@ -111,36 +91,12 @@ AFRAME.registerComponent( 'pickup-object-component',{
 
         camera.object3D.localToWorld(Context_AF.el.object3D.position);
         camera.object3D.add(Context_AF.el.object3D);
-        camera.object3D.add(ball.object3D)
+        Context_AF.el.removeAttribute('dynamic-body');
+        camera.object3D.add(ball.object3D);
         Context_AF.el.setAttribute('position', {x: 0, y: 0, z:-2});
         Picked_UP = true;
         console.log("item picked up");
         
-    },
-    dropBall :function(){
-        const Context_AF = this;
-        let camera = document.getElementById('camera');
-
-        let wVec = camera.object3D.localToWorld(Context_AF.el.object3D.position);
-
-        Context_AF.el.parentNode.removeChild(Context_AF.el);
-        console.log("placing item");
-        Picked_UP = false;
-
-        // creating a new object in the position of the removed object
-        let grabElem = document.createElement('a-entity');
-        grabElem.position = "0 1 0";
-        //grabElem.setAttribute('position',{x: wVec.x, y:wVec.y, z:wVec.z});
-        grabElem.setAttribute('id', 'ball'); 
-        grabElem.setAttribute('class', 'clickable');
-        grabElem.setAttribute('geometry',{primitive:'sphere', radius:'0.5', height:'1.0'});
-        grabElem.setAttribute('material','color:yellow;');
-        grabElem.setAttribute('pickup-object-component','');
-
-        let scene = document.querySelector('a-scene');
-        scene.appendChild(grabElem);
-
-
-        
     }
+   
 });
